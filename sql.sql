20f0c950e1670f752b7c3904fdbd863108031f8c
Jessica <jessicakorubo@gmail.com>
2024-01-17 08:25:54 +0100

    Imported and updated SQL file

--- sql.sql
+++ sql.sql
@@ -1,63 +1,199 @@
-create table if not exists `products` (
-    `product_id` int(11) not null auto_increment,
-    `product_name` varchar(100) not null,
-    `product_category` varchar(100) not null,
-    `product_description` varchar(255) not null,
-    `product_image` varchar(255) not null,
-    `product_image2` varchar(255) not null,
-    `product_image3` varchar(255) not null,
-    `product_image4` varchar(255) not null,
-    `product_price` decimal(6, 2) not null,
-    `product_special_offer` integer(2) not null,
-    `product_color` varchar(100) not null,
-    PRIMARY KEY (`product_id`)
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 17, 2024 at 08:24 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-create table if not exists `orders` (
-    `order_id` int(11) NOT NULL AUTO_INCREMENT,
-    `order_cost` decimal(6,2) NOT NULL,
-    `order_status` varchar(100) NOT NULL DEFAULT 'on_hold',
-    `user_id` int(11) NOT NULL,
-    `user_phone` int(11) NOT NULL,
-    `user_city` varchar(255) NOT NULL,
-    `user_address` varchar(255) NOT NULL,
-    `order_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`order_id`)
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table if not exists `order_items` (
-    `item_id` int(11) NOT NULL AUTO_INCREMENT,
-    `order_id` int(11) NOT NULL,
-    `product_id` varchar(255) NOT NULL,
-    `product_name` varchar(255) NOT NULL,
-    `product_image` varchar(255) NOT NULL,
-    `user_id` int(11) NOT NULL,
-    `order_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`item_id`)
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+--
+-- Database: `ecommerce`
+--
 
-create table if not exists `users` (
-    `user_id` int(11) NOT NULL AUTO_INCREMENT,
-    `user_name` varchar(100) NOT NULL,
-    `user_email` varchar(100) NOT NULL,
-    `user_password` varchar(100) NOT NULL,
-    PRIMARY KEY (`user_id`),
-    UNIQUE KEY `UX_Constraint` (`user_email`)
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `admin`
+--
 
-create table if not exists `admin` (
-    `username` varchar(100) NOT NULL,
-    `password` varchar(100) NOT NULL
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `admin` (
+  `admin_username` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `admin`
+--
 
-create table if not exists `category` (
-    `category_id` int AUTO_INCREMENT NOT NULL,
-    `category_name` varchar(100),
-    PRIMARY KEY (`category_id`)
-)ENGINE InnoDB DEFAULT CHARSET= utf8;
+INSERT INTO `admin` (`admin_username`, `admin_password`) VALUES
+('admin', 'admin'),
+('admin', 'admin');
 
+-- --------------------------------------------------------
 
-INSERT INTO `admin` (`username`, `password`) VALUES ('admin', 'admin');
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Bags\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `order_cost` decimal(6,2) NOT NULL,
+  `order_status` varchar(100) NOT NULL DEFAULT 'on_hold',
+  `user_id` int(11) NOT NULL,
+  `user_phone` int(11) NOT NULL,
+  `user_city` varchar(255) NOT NULL,
+  `user_address` varchar(255) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_category` varchar(100) NOT NULL,
+  `product_description` varchar(255) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `product_image2` varchar(255) NOT NULL,
+  `product_image3` varchar(255) NOT NULL,
+  `product_image4` varchar(255) NOT NULL,
+  `product_price` decimal(6,2) NOT NULL,
+  `product_special_offer` int(2) NOT NULL,
+  `product_color` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `UX_Constraint` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
